f1de4c4467c3ac90de33791a5122744755f71e0c
Silvio Luiz Stanzani <silvio.stanzani@gmail.com>
2016-09-19 13:13:39 -0300

    Update autovec.c

--- Vectorization/autovec.c
+++ Vectorization/autovec.c
@@ -2,25 +2,18 @@
 #include <stdio.h>
 
 int main(){
-  const int n=90000;
-  int i, j, randV;
-  int A[n];
-  int B[n];
+    const int n=90000;
+    int i, j, randV;
+    int A[n];
+    int B[n];
 
+    for (j=0; j<45000; j++) {
+        srand(time(NULL));
+        randV=rand();
 
-  for (j=0; j<45000; j++) {
-srand(time(NULL));
-randV=rand();
-
-  for (i=0; i<n; i++)
-    A[i]=B[i]=i*randV;
-
-  // This loop will be auto-vectorized
-  for (i=0; i<n; i++)
-    A[i]+=B[i]*randV;
-
-}
-
-//  for (i=0; i<n; i++)
-  //  printf("%2d %2d %2d\n", i, A[i], B[i]);
+        for (i=0; i<n; i++)
+            A[i]=B[i]=i*randV;
+        //    for (i=0; i<n; i++)
+        //        A[i]+=B[i]*randV;
+    }
 }
